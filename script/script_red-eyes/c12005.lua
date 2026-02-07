8dbae9d6456d6129806a4a216d7ee64b95384c19
피딮 <128398860+vlelv-github@users.noreply.github.com>
2026-02-08 02:34:16 +0900

    집결한 우정 버그 수정
    "집결한 우정"의 2의 효과가, 엑시즈 소환에 대응하여 발동되지 않던 점을 수정
    2026-02-08

--- script/script_red-eyes/c12005.lua
+++ script/script_red-eyes/c12005.lua
@@ -12,15 +12,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	e1:SetCountLimit(1, id) -- 1턴에 1번 발동 가능
 	c:RegisterEffect(e1)
-	-- check
-	local e2 = Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_RANGE)
-	e2:SetCode(EFFECT_MATERIAL_CHECK)
-	e2:SetRange(LOCATION_GRAVE)
-	e2:SetValue(s.valcheck)
-	c:RegisterEffect(e2)
-	-- search
+
 	local e3 = Effect.CreateEffect(c)
 	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
@@ -36,19 +28,15 @@ function s.initial_effect(c)
 end
 s.listed_series={0x3b}
 s.listed_name={id}
-function s.valcheck(e, c)
-	if not c:IsSummonPlayer(e:GetHandlerPlayer()) then return end --자신이 소환했을 때에만 유효
-	local g = c:GetMaterial() --소재 정보 가져옴
-	if not g or #g == 0 then return end --소재 정보가 있을 때에만 유효
-	if g:IsExists(Card.IsType, 1, nil, TYPE_NORMAL) then
-		c:RegisterFlagEffect(id, RESET_CHAIN, 0, 1)
-	end
+
+function s.cfilter(c,tp)
+	return (c:IsXyzSummoned() or c:IsFusionSummoned()) and c:IsSummonPlayer(tp) and c:GetMaterial():IsExists(s.matfilter,1,nil)
 end
-function s.cfilter(c)
-	return c:GetFlagEffect(id)>0 and (c:IsSummonType(SUMMON_TYPE_FUSION) or c:IsSummonType(SUMMON_TYPE_XYZ))
+function s.matfilter(c)
+	return c:IsType(TYPE_NORMAL)
 end
 function s.cond(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(function(c) return c:IsSummonPlayer(tp) and s.cfilter(c) end, 1, nil)
+	return eg:IsExists(s.cfilter,1,nil,tp)
 end
 function s.ft(c, e, tp)
 	return (c:IsSetCard(0x3b) and c:IsSpellTrap() and not c:IsCode(id))
